cd5f83eca251c6a6c1765432b07cd06afe72087d
Bill Derouin <derouinbill@gmail.com>
2016-02-02 20:52:01 -0800

    Twilio test

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ app.get('/', function(req, res) {
     res.end('Hello, phonebuzz');
 });
 
+// Phase 1
+app.get('/phonebuzz', function(req, res) {
+    // Return a phone voice prompt for user to enter number
+    // Encoded in TwiML
+    var response = '<?xml version="1.0" encoding="UTF-8"?><Response><Say>Please enter a number</Say></Response>';
+    res.end(response);
+});
+
 // Start the server
-var PORT = 1337;
-var server = app.listen(PORT, function() {
+var port = process.env.PORT || 8080;
+var server = app.listen(port, function() {
     var host = server.address().address;
     var port = server.address().port;
 
